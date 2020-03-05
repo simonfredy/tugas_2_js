7fda83e8b01f3ea22e7ccf2d2d715b6fd67c413a
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-05 14:30:50 +0700

    Tugas 2 JS

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var angka = 30;
+var str = "Selamat";
+var bool = true;
+var nl = null;
+var und; 
+var ary = [5, 3, 7, 9, 0, 1, 8, 2];
+
+console.log(angka);
+console.log(str);
+console.log(bool);
+console.log(nl);
+console.log(und);
+console.log(ary);
